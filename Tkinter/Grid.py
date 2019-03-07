0c42eabe6faa2c70a9fe1f361c0a6c2a2426f325
Nikolai Timofeev <extezy34@gmail.com>
2019-03-07 22:40:32 +0300

    упакощик Grid

---
+++ Tkinter/Grid.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()  # базовое окно нашего приложения
+
+label_1 = Label(root, text='Name')
+label_2 = Label(root, text='Password')
+
+entry_1 = Entry(root)
+entry_2 = Entry(root)
+
+label_1.grid(row=0, column=0, sticky=E)
+label_2.grid(row=1, column=0)
+
+entry_1.grid(row=0, column=1)
+entry_2.grid(row=1, column=1)
+
+c = Checkbutton(root, text='Stay logged in the system')
+c.grid(columnspan=2)
+
+root.mainloop()  # зацикливание приложения
